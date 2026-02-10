839965afa00fc4723407f0ac5beebe5b0361f8af
Fran Ferrer <franefun@gmail.com>
2026-02-10 20:00:54 +0100

    first commit

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { HelloCdkStack } from '../lib/hello-cdk-stack';
+const id = process.env.VITE_ID;
 
 const app = new cdk.App();
 new HelloCdkStack(app, 'HelloCdkStack', {
@@ -14,7 +15,8 @@ new HelloCdkStack(app, 'HelloCdkStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+   env: { account: id, region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,4 +1,4 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +6,26 @@ export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /*Crear constructor de nivel 1 (L1)
+    
+    -------------------------------------------------------------------------------
+    new cdk.aws_s3.CfnBucket(this, 'MyFirstBucketFFT', {
+      bucketName: 'my-first-bucket-fft'
+    })
+   -------------------------------------------------------------------------------
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    Crear constructor de nivel 2 (L2)
+
+    -------------------------------------------------------------------------------
+    const bucket = new cdk.aws_s3.Bucket(this, 'MyFirstBucketFFT2', {
+      bucketName: 'my-first-bucket-fft2'
+    })
+    new cdk.aws_s3_deployment.BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [cdk.aws_s3_deployment.Source.asset('./files')],
+      destinationBucket: bucket,
+    })
+    -------------------------------------------------------------------------------
+    */
   }
+
 }
